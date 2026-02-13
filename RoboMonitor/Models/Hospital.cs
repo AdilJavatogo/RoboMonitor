b9a2e7d9d3bcad183585d6a07e5386ef11099cc5
AdilJavatogo <Adil2300@hotmail.com>
2026-02-13 09:23:31 +0100

    Model Hospital

---
+++ RoboMonitor/Models/Hospital.cs
@@ -0,0 +1,12 @@
+﻿namespace RoboMonitor.Models
+{
+    public class Hospital
+    {
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
