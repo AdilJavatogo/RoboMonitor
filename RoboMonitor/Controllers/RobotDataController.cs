b156ef866caf4729b1fee82e0a75171e29fb8320
AdilJavatogo <Adil2300@hotmail.com>
2026-03-03 13:59:44 +0100

    RobotDataController

---
+++ RoboMonitor/Controllers/RobotDataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoboMonitor.Models;
+
+namespace RoboMonitor.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RobotDataController : Controller
+    {
+        [HttpPost]
+        public IActionResult PostRobotData([FromBody] Robot robot)
+        {
+            // Her kan du tilføje logik til at gemme eller behandle robotdataen
+            // For eksempel, gemme dataen i en database eller sende den til en anden service
+            // Returner en succesrespons
+
+            Console.WriteLine($"Data mnodtaget: {robot.BatteryLevel}"); // robot.Id osv.
+
+            return Ok();
+        }
+    }
+}
