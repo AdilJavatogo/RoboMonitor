b1b85fd2448986198ca8738732f5126e0b52c6a6
AdilJavatogo <Adil2300@hotmail.com>
2026-03-05 11:02:47 +0100

    ....

--- RoboMonitor/Program.cs
+++ RoboMonitor/Program.cs
@@ -5,36 +5,9 @@ using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-//var serviceName = "RobotMonitor";
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-//builder.Services.AddOpenTelemetry()
-//    .ConfigureResource(resource => resource
-//        .AddService(serviceName: builder.Environment.ApplicationName))
-
-//    .WithTracing(tracing =>
-//            {
-//                tracing
-//                .AddAspNetCoreInstrumentation()
-//                .AddHttpClientInstrumentation()
-//                .AddSqlClientInstrumentation();
-
-//            })
-
-//    .WithMetrics(metrics =>
-//           {
-//               metrics
-//               .AddAspNetCoreInstrumentation()
-//               .AddHttpClientInstrumentation()
-//               .AddRuntimeInstrumentation();
-//               //.AddPrometheusExporter();
-
-//           });
 
 
 builder.AddServiceDefaults();
