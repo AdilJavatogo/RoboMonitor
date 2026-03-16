d3cdca9cfcacf2983121ba9919857a3b0f69e366
AdilJavatogo <Adil2300@hotmail.com>
2026-03-16 11:40:58 +0100

    ...

--- AspireRoboMonitor/AppHost.cs
+++ AspireRoboMonitor/AppHost.cs
@@ -7,12 +7,12 @@ var api = builder.AddProject<Projects.RoboMonitor>("robotmonitor")
     .WithExternalHttpEndpoints();
 
 var prometheus = builder.AddContainer("prometheus", "prom/prometheus")
-       .WithBindMount("./prometheus.yml", "/etc/prometheus/prometheus.yml")
+       .WithBindMount("./prometheus_config/prometheus.yml", "/etc/prometheus/prometheus.yml")
        .WithHttpEndpoint(port: 9090, targetPort: 9090, name: "prom-http")
        .WaitFor(api);
 
 var grafana = builder.AddContainer("grafana", "grafana/grafana")
-       .WithBindMount("./grafana-datasource.yaml", "/etc/grafana/provisioning/datasources/datasource.yaml")
+       .WithBindMount("./grafana_config/grafana-datasource.yaml", "/etc/grafana/provisioning/datasources/datasource.yaml")
        .WithHttpEndpoint(port: 3000, targetPort: 3000)
        .WithExternalHttpEndpoints()
        .WaitFor(prometheus); // Vent på Prometheus
