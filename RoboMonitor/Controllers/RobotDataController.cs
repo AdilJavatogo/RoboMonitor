e659f01f3258c943e8d8ce11d13b3c81b81ec648
AdilJavatogo <Adil2300@hotmail.com>
2026-03-18 11:58:56 +0100

    removed comment

--- RoboMonitor/Controllers/RobotDataController.cs
+++ RoboMonitor/Controllers/RobotDataController.cs
@@ -11,7 +11,6 @@ namespace RoboMonitor.Controllers
 
         private readonly IRobotRepository _repository;
 
-        // Dependency Injection (SOLID: Inversion of Control)
         public RobotDataController(IRobotRepository repository)
         {
             _repository = repository;
@@ -20,8 +19,6 @@ namespace RoboMonitor.Controllers
         [HttpPost]
         public IActionResult PostData([FromBody] Robot robot)
         {
-            // Vi smider hele robot-objektet direkte i databasen. 
-            // Slut med at opdatere properties én for én!
             _repository.UpsertRobot(robot);
 
             Console.WriteLine($"[UPDATE] Robot {robot.RobotId}: Batteri {robot.BatteryLevel}%, State: {robot.RobotState}");
@@ -33,47 +30,5 @@ namespace RoboMonitor.Controllers
         {
             return _repository.GetAllRobots();
         }
-
-        //[HttpPost]
-        //public IActionResult PostRobotData([FromBody] Robot robot)
-        //{
-        //    // Prometheus data modtaget - opdater robotten i listen
-
-
-        //    Console.WriteLine($"Data mnodtaget: {robot.BatteryLevel}"); // robot.Id osv.
-
-        //    return Ok();
-        //}
-
-
-        //[HttpPost]
-        //public IActionResult PostData([FromBody] Robot robot)
-        //{
-        //    var robotList = RobotController._robots.FirstOrDefault(r => r.RobotId == robot.RobotId);
-
-        //    if (robotList != null)
-        //    {
-        //        // OPDATERE Grafana via den anden liste  - DETTE SKAL NOK LAVES TIL SIN EGEN LISTE SENERE
-        //        robotList.BatteryLevel = robot.BatteryLevel;
-        //        robotList.CPUTemperature = robot.CPUTemperature;
-        //        robotList.RobotStatus = robot.RobotStatus;
-        //        robotList.ChargingTime = robot.ChargingTime;
-        //        robotList.EStop = robot.EStop;
-        //        robotList.Lift = robot.Lift;
-        //        robotList.BreakCount = robot.BreakCount;
-        //        robotList.Department = robot.Department;
-        //        robotList.Distance = robot.Distance;
-
-        //        Console.WriteLine($"[UPDATE] Robot {robot.RobotId}: Batteri {robot.BatteryLevel}%");
-        //    }
-        //    else
-        //    {
-        //        // Tilføj ny robot til listen
-        //        RobotController._robots.Add(robot);
-        //        Console.WriteLine($"ID {robot.RobotId} er nu registreret i systemet!");
-        //    }
-
-        //    return Ok();
-        //}
     }
 }
