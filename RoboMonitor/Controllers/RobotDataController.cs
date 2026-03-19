b5ae07f233f75bb283bcc2141d27a7fb90423659
AdilJavatogo <Adil2300@hotmail.com>
2026-03-19 11:41:10 +0100

    fjernet kommentar

--- RoboMonitor/Controllers/RobotDataController.cs
+++ RoboMonitor/Controllers/RobotDataController.cs
@@ -30,5 +30,36 @@ namespace RoboMonitor.Controllers
         {
             return _repository.GetAllRobots();
         }
+
+
+        //[HttpPost]
+        //public IActionResult PostData([FromBody] Robot robot)
+        //{
+        //    var robotList = RobotController._robots.FirstOrDefault(r => r.RobotId == robot.RobotId);
+
+        //    if (robotList != null)
+        //    {
+        //        // OPDATERE Grafana via den anden liste  - DETTE SKAL NOK LAVES TIL SIN EGEN LISTE SENERE
+        //        robotList.BatteryLevel = robot.BatteryLevel;
+        //        robotList.CPUTemperature = robot.CPUTemperature;
+        //        robotList.RobotStatus = robot.RobotStatus;
+        //        robotList.ChargingTime = robot.ChargingTime;
+        //        robotList.EStop = robot.EStop;
+        //        robotList.Lift = robot.Lift;
+        //        robotList.BreakCount = robot.BreakCount;
+        //        robotList.Department = robot.Department;
+        //        robotList.Distance = robot.Distance;
+
+        //        Console.WriteLine($"[UPDATE] Robot {robot.RobotId}: Batteri {robot.BatteryLevel}%");
+        //    }
+        //    else
+        //    {
+        //        // Tilføj ny robot til listen
+        //        RobotController._robots.Add(robot);
+        //        Console.WriteLine($"ID {robot.RobotId} er nu registreret i systemet!");
+        //    }
+
+        //    return Ok();
+        //}
     }
 }
